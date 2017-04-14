ad7751ba3fd33cf28cb1f276b93d562183331ec2
Luis Menina <liberforce@freeside.fr>
2017-04-14 15:58:27 +0200

    Relicense to MIT, add space after comment sign

--- svn-hacks.sh
+++ svn-hacks.sh
@@ -1,22 +1,20 @@
-#Author:  Martin Grenfell [http://github.com/scrooloose]
-#License:
+# Copyright (C) 2010, Martin Grenfell [http://github.com/scrooloose]
+# Copyright (C) 2017, Luis Menina <liberforce AT freeside DOT fr>
 #
-#  This program is free software. It comes without any warranty, to
-#  the extent permitted by applicable law. You can redistribute it
-#  and/or modify it under the terms of the Do What The Fuck You Want
-#  To Public License, Version 2, as published by Sam Hocevar.  See
-#  http://sam.zoy.org/wtfpl/COPYING for more details.
+# SPDX-License-Identifier: MIT
+#-------------------------------------------------------------------
+# Purpose:
 #
-#This script hijacks calls to svn and adds color and pagination to
-#some svn commands. Source it from your bashrc.
+# This script hijacks calls to svn and adds color and pagination to
+# some svn commands. Source it from your bashrc.
 #
-#colordiff must be installed.
+# colordiff must be installed.
 #-------------------------------------------------------------------
 
-#intercept calls to svn
+# intercept calls to svn
 svn () {
 
-  #bail if the user didnt specify which subversion command to invoke
+  # bail if the user didnt specify which subversion command to invoke
   if [ $# -lt 1 ]; then
     command svn
     return
@@ -25,12 +23,12 @@ svn () {
   local sub_cmd=$1
   shift
 
-  #intercept svn diff commands
+  # intercept svn diff commands
   if [ $sub_cmd == diff ]; then
 
-    #colorize the diff
-    #remove stupid ^M dos line endings
-    #page it if there's more one screen
+    # colorize the diff
+    # remove stupid ^M dos line endings
+    # page it if there's more one screen
     command svn diff "$@" | colordiff | sed -e 's/\r//g' | less -RF --no-init
 
   elif [ $sub_cmd == 'format-patch' ]; then
@@ -39,13 +37,13 @@ svn () {
     # patches easier to adapt
     command svn diff -x '--unified --show-c-function' --patch-compatible "$@" | sed -e 's;^--- ;&a/;' -e 's;^+++ ;&b/;'
 
-  #add some color to svn status output and page if needed:
-  #M = blue
-  #A = green
-  #D/!/~ = red
-  #C = magenta
+  # add some color to svn status output and page if needed:
+  # M = blue
+  # A = green
+  # D/!/~ = red
+  # C = magenta
   #
-  #note that C and M can be preceded by whitespace - see $svn help status
+  # note that C and M can be preceded by whitespace - see $svn help status
   elif [[ $sub_cmd =~ ^(status|st)$ ]]; then
     command svn status "$@" | sed -e 's/^\(\([A-Z]\s\+\(+\s\+\)\?\)\?C .*\)$/\o33\[1;35m\1\o33[0m/' \
                                   -e 's/^\(\s*M.*\)$/\o33\[1;34m\1\o33[0m/' \
@@ -61,15 +59,15 @@ svn () {
   elif [[ $sub_cmd =~ ^(blame|help|h|cat)$ ]]; then
     command svn $sub_cmd "$@" | less -F
 
-  #colorize and page svn log
-  #rearrange the date field from:
-  #  2010-10-08 21:19:24 +1300 (Fri, 08 Oct 2010)
-  #to:
-  #  2010-10-08 21:19 (Fri, +1300)
+  # colorize and page svn log
+  # rearrange the date field from:
+  #   2010-10-08 21:19:24 +1300 (Fri, 08 Oct 2010)
+  # to:
+  #   2010-10-08 21:19 (Fri, +1300)
   elif [[ $sub_cmd == log ]]; then
     command svn log "$@" | sed -e 's/^\(.*\)|\(.*\)| \(.*\) \(.*\):[0-9]\{2\} \(.*\) (\(...\).*) |\(.*\)$/\o33\[1;32m\1\o33[0m|\o33\[1;34m\2\o33[0m| \o33\[1;35m\3 \4 (\6, \5)\o33[0m |\7/' | less -RF --no-init
 
-  #let svn handle it as normal
+  # let svn handle it as normal
   else
     command svn $sub_cmd "$@"
   fi
