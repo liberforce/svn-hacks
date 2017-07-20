3de187f717950dac1549b415b7b0b2f1dc867d96
Luis Menina <liberforce@freeside.fr>
2017-07-20 14:10:09 +0200

    Add usage, use functions

--- gen-authors-file.sh
+++ gen-authors-file.sh
@@ -1,16 +1,42 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-if [ $# -gt 0 ]; then
-  repo="$1"
-else
-  repo=""
-fi
+usage()
+{
+    cat << EOF
+Usage: $(basename "$0") REPO_PATH_OR_URL
 
-authors=$(svn log "$repo" -q | grep -e '^r' | awk 'BEGIN { FS = "|" } ; { print $2 }' | sort | uniq)
-for author in ${authors}; do
-	name=$(finger -m ${author} 2> /dev/null | sed -ne "s/.*Name: \(.*\)/\1/gp")
-	if [ -z "${name}" ] ; then
-		name="Unknown"
-	fi
-	echo "${author} = ${name} <USER@DOMAIN>";
-done | sort -u
+Outputs the list of users needed to generate an authors file usable by git-svn
+
+REPO_PATH_OR_URL      Local path or URL to your local svn repository.
+EOF
+
+}
+
+get_author_name()
+{
+    local author="$1"
+    echo $(finger -m ${author} 2> /dev/null | sed -ne "s/.*Name: \(.*\)/\1/gp")
+}
+
+main()
+{
+    local repo=""
+
+    if [ $# -eq 1 ]; then
+        repo="$1"
+    else
+        usage
+        exit 1
+    fi
+
+    authors=$(svn log "$repo" -q | grep -e '^r' | awk 'BEGIN { FS = "|" } ; { print $2 }' | sort | uniq)
+    for author in ${authors}; do
+            local name=$(get_author_name "${author}")
+            if [ -z "${name}" ] ; then
+                    name="Unknown"
+            fi
+            echo "${author} = ${name} <USER@DOMAIN>";
+    done | sort -u
+}
+
+main "$@"
