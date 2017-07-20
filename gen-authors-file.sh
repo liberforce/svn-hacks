df47621b79f88be1fd67b6d25121631b27a9789c
Luis Menina <liberforce@freeside.fr>
2017-07-20 14:32:03 +0200

    Automatically detect and update the authors file

--- gen-authors-file.sh
+++ gen-authors-file.sh
@@ -29,6 +29,16 @@ main()
         exit 1
     fi
 
+    authorsfile=$(git config svn.authorsfile)
+    if [ ! -f "$authorsfile" ]; then
+        echo "Error: Please configure location of your authors file first using:" >&2
+        echo "   git config --global svn.authorsfile /location/to/your/authors/file" >&2
+        exit 1
+    fi
+
+    local tmpfile=$(mktemp)
+    cp "$authorsfile" "$tmpfile"
+
     authors=$(svn log "$repo" -q | grep -e '^r' | awk 'BEGIN { FS = "|" } ; { print $2 }' | sort | uniq)
     for author in ${authors}; do
             local name=$(get_author_name "${author}")
@@ -36,7 +46,8 @@ main()
                     name="Unknown"
             fi
             echo "${author} = ${name} <USER@DOMAIN>";
-    done | sort -u
+    done >> "$tmpfile"
+    cat "$tmpfile" | sort -u > "$authorsfile"
 }
 
 main "$@"
